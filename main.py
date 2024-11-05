0076e4d3becc590856456e0e3733ff7a24ec3ea0
Jafar-gif <jafarortukov97@gmail.com>
2024-11-05 18:26:14 +0300

    Home_worck

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class House:
 
+    def __init__(self,name,number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        def go_to(self,new_floor):
+            if 1 < new_floor < self.number_of_floors:
+                for i in range(1, new_floor + 1):
+                    print(i)
+                else:
+                    ('Такого этажа не существует')
 
+    def __len__(self):
+        return self.number_of_floors
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def __str__(self):
+        return f'Название:{self.name},количество этажей:{self.number_of_floors}'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+h1 = House('Жк Эльбрус',10)
+h2 = House('Жк Акация',20)
+print(h1)
+print(h2)
+
+print(len(h1))
+print(len(h2))
